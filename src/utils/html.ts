e2a3dfaff448f41596af29d6a7321d31702f98cd
samet <sametcn99@gmail.com>
2024-05-27 07:21:27 +0300

    chore: Refactor HTML parsing and rendering

--- src/utils/html.ts
+++ src/utils/html.ts
@@ -13,11 +13,16 @@ export async function getReadme() {
   const decodedReadmeContent = textDecoder.decode(readmeContent);
 
   const parsedHtml = parseHtml(decodedReadmeContent) as unknown as Document;
-  addTitleToHtml(parsedHtml, "Reddit RSS API");
-  addStylesToHtml(parsedHtml);
-  addMetaTagsToHtml(parsedHtml);
+  const dom = await getIndexHtml();
+  dom.body.innerHTML = parsedHtml.body.innerHTML;
+  return dom;
+}
 
-  return parsedHtml;
+export async function getIndexHtml() {
+  const index = await Deno.readFile("index.html");
+  const textDecoder = new TextDecoder("utf-8");
+  const decodedIndex = textDecoder.decode(index);
+  return parseHtml(decodedIndex) as unknown as Document;
 }
 
 /**
@@ -35,98 +40,4 @@ function parseHtml(content: string) {
     throw new Error(`Failed to parse the content.`);
   }
   return parsedHtml;
-}
-
-/**
- * Adds a title element to the HTML document.
- *
- * @param parsedHtml - The parsed HTML document.
- * @param title - The title to be added.
- */
-function addTitleToHtml(parsedHtml: Document, title: string) {
-  const titleElement = parsedHtml.createElement("title");
-  titleElement.textContent = title;
-  parsedHtml.head.appendChild(titleElement);
-}
-
-/**
- * Adds styles to the HTML document.
- * @param parsedHtml - The parsed HTML document.
- */
-function addStylesToHtml(parsedHtml: Document) {
-  const BACKGROUND_COLOR = "#0C0C0C";
-  const TEXT_COLOR = "#fff";
-  const MAX_WIDTH = "800px";
-  const FONT_FAMILY = "Arial, sans-serif";
-  const LINK_COLOR = "#002DCE";
-
-  const styleElement = parsedHtml.createElement("style");
-  styleElement.textContent = `
-    body {
-      background-color: ${BACKGROUND_COLOR};
-      color: ${TEXT_COLOR};
-      max-width: ${MAX_WIDTH};
-      margin: auto;
-      font-family: ${FONT_FAMILY};
-    }
-    a {
-      color: ${LINK_COLOR};
-      font-weight: bold;
-      font-family: ${FONT_FAMILY};
-      text-decoration: none;
-    }
-    a:hover {
-      text-decoration: underline;
-    }
-    code {
-      background-color: #808080;
-      padding-right: 5px;
-      padding-left: 5px;
-      border-radius: 8px;
-    }
-  `;
-  parsedHtml.head.appendChild(styleElement);
-}
-
-/**
- * Adds meta tags to the HTML document.
- *
- * @param parsedHtml - The parsed HTML document.
- */
-function addMetaTagsToHtml(parsedHtml: Document) {
-  const metaTags = [
-    { name: "title", content: "Reddit RSS API" },
-    {
-      name: "description",
-      content:
-        "An API for fetching Reddit posts from RSS feeds. It offers multiple endpoints to retrieve posts from one or more subreddits.",
-    },
-    { name: "keywords", content: "reddit rss, reddit api" },
-    { name: "robots", content: "index, follow" },
-    { httpEquiv: "Content-Type", content: "text/html; charset=utf-8" },
-    { name: "language", content: "English" },
-    { name: "author", content: "sametcn99" },
-    { name: "viewport", content: "width=device-width, initial-scale=1" },
-  ];
-
-  const favicon = parsedHtml.createElement("link");
-  favicon.setAttribute("rel", "icon");
-  favicon.setAttribute("type", "image/png");
-  favicon.setAttribute(
-    "href",
-    "https://raw.githubusercontent.com/sametcn99/reddit-rss-api/main/public/favicon.ico"
-  );
-  parsedHtml.head.appendChild(favicon);
-  for (const metaTag of metaTags) {
-    const metaElement = parsedHtml.createElement("meta");
-
-    if (metaTag.name) {
-      metaElement.setAttribute("name", metaTag.name);
-    }
-    if (metaTag.httpEquiv) {
-      metaElement.setAttribute("http-equiv", metaTag.httpEquiv);
-    }
-    metaElement.setAttribute("content", metaTag.content);
-    parsedHtml.head.appendChild(metaElement);
-  }
 }
